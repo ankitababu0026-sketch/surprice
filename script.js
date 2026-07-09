476467ed583249a29cc74bbbb5c1803b0164e155
ankitababu0026-sketch <ankitababu0026@gmail.com>
2026-07-09 14:28:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function moveButton() {
     const noBtn = document.getElementById('noBtn');
-    
-    // बटन को पूरी स्क्रीन पर कहीं भी रैंडमली भेजने के लिए
     const x = Math.random() * (window.innerWidth - 100);
     const y = Math.random() * (window.innerHeight - 50);
     
-    noBtn.style.position = 'absolute';
     noBtn.style.left = x + 'px';
     noBtn.style.top = y + 'px';
 }
