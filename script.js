74a3a25bde3722287a0356bc09ba5af7045f5881
ankitababu0026-sketch <ankitababu0026@gmail.com>
2026-07-10 13:39:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+function createBalloons() {
+    const container = document.getElementById('balloons-container');
+    for(let i=0; i<10; i++) {
+        let b = document.createElement('div');
+        b.className = 'balloon';
+        b.innerHTML = i % 2 == 0 ? '🎈' : '❤️';
+        b.style.left = Math.random() * 100 + '%';
+        b.style.animationDelay = Math.random() * 5 + 's';
+        container.appendChild(b);
+    }
+}
+createBalloons();
+
 function showGift() {
     document.getElementById('giftPopup').style.display = 'flex';
-    confetti(); 
+    confetti();
 }
 function moveButton() {
     const noBtn = document.getElementById('noBtn');
-    noBtn.style.left = (Math.random() * (window.innerWidth - 120)) + 'px';
-    noBtn.style.top = (Math.random() * (window.innerHeight - 80)) + 'px';
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = Math.random() * (window.innerWidth - 80) + 'px';
+    noBtn.style.top = Math.random() * (window.innerHeight - 50) + 'px';
 }
